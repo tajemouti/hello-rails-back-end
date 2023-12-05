ebd80d7841d449ccb84f5c46efd963b3124972c6
tajemouti <tajemouti@gmail.com>
2023-12-05 16:24:03 +0100

    Set up root route

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::GreetingsController < ApplicationController
   def index
-    render json: { greeting: Greeting.order("RANDOM()").first.content }
+    render json: { greeting: Greeting.order('RANDOM()').first.content }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,6 @@ Rails.application.routes.draw do
       get 'greetings/index'
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root "api/v1/greetings#index"
 end
